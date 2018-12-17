6176c2d6f602d0569476fab930418c7076efa3e3
Bruna de Oliveira Barbosa <bruna@Brunas-MacBook-Pro.local>
2018-12-17 01:43:15 -0300

    inicio do test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import Adapter from "enzyme-adapter-react-16";
+import { shallow, configure } from "enzyme";
+import ShowFlashcard from "./components/showFlashcardComponent/ShowFlashcard";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+describe("<ShowFlashcard />", () => {
+  it("renders a title", () => {
+    const view = shallow(<ShowFlashcard />);
+    expect(view.find("h1").length).toEqual(1);
+  });
+
+  it("renders a body", () => {
+    const view = shallow(<ShowFlashcard />);
+    expect(view.find("p").length).toEqual(1);
+  });
 });
